6a176d99c6f23d0e5c37bbb35cf54a17b7ca6277
gokul.karunaharan <gokul.karunaharan@capestart.com>
2026-04-06 23:20:27 -0400

    debug commit

--- src/main/java/com/gokul/spring_reactive_mongodb/config/SecurityConfig.java
+++ src/main/java/com/gokul/spring_reactive_mongodb/config/SecurityConfig.java
@@ -1,24 +1,39 @@
 package com.gokul.spring_reactive_mongodb.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        return NimbusReactiveJwtDecoder
+                .withJwkSetUri(this.jwkSetUri)
+                .build();
+    }
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(
-            ServerHttpSecurity http) {
+            ServerHttpSecurity http,ReactiveJwtDecoder reactiveJwtDecoder) {
         http
                 .authorizeExchange(spec ->
                         spec.anyExchange().authenticated())
                 .oauth2ResourceServer(spec ->
-                        spec.jwt(Customizer.withDefaults()));
+                        spec.jwt(jwt ->
+                                jwt.jwtDecoder(reactiveJwtDecoder)));
         return http.build();
     }
+
 }
